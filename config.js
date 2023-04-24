a74b5379ce711cf6071b82d52662448d961e7d29
tsmine <131702493+tsmine@users.noreply.github.com>
2023-04-24 18:52:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 module.exports = {
     // ...
     env: {
-        token: "", // your bot token
-        channel: "", // channel id from the channel the embed should get send to
-        message: "", // message id from the message the system has send into the channel above
+        token: "MTEwMDEwNDUzOTEyMzc1NzEzNg.G5rqwL.dAE8kge1F17dd2mzjz9bMbkB5Gl3zxUQBv1_AY", // your bot token
+        channel: "1081291226579472498", // channel id from the channel the embed should get send to
+        message: "hii hii hii roles", // message id from the message the system has send into the channel above
     },
     
     status: {
-        type: "", // your status of the bot (WATCHING, LISTENING, PLAYING, STREAMING)
-        activity: "", // your activity of the bot
+        type: "LISTENING", // your status of the bot (WATCHING, LISTENING, PLAYING, STREAMING)
+        activity: "to marina", // your activity of the bot
     },
 
     roles: [
         role = {
-            id: "", //id of the role
-            emoji: "", // emoji for the role example: ⚫
+            id: "1081291330698879196", //id of the role
+            emoji: "⚫", // emoji for the role example: ⚫
         },
         
     ],
